97f490f6b6d2adf6affcbb79665d2f813870b065
trungnam-c <namn71201@gmail.com>
2022-02-16 10:33:14 +0700

    push

--- sidebar.php
+++ sidebar.php
@@ -70,30 +70,23 @@ require_once($_SERVER['DOCUMENT_ROOT'] . '/greetingcard/language/language.php');
 						<label>Navigation</label>
 					</li> -->
                     <li class="nav-item">
-                        <a href="index.php" class="nav-link"><span class="pcoded-micon"><i
-                                    class="feather icon-home"></i></span><span
-                                class="pcoded-mtext"><?php echo $dash_board; ?></span></a>
+                        <a href="index.php" class="nav-link"><span class="pcoded-micon"><i class="feather icon-home"></i></span><span class="pcoded-mtext"><?php echo $dash_board; ?></span></a>
                     </li>
                     <li class="nav-item">
-                        <a href="./modules/customer.php" class="nav-link"><span class="pcoded-micon"><i
-                                    class="feather icon-users"></i></span><span
-                                class="pcoded-mtext"><?php echo $customer; ?></span></a>
+                        <a href="./modules/customer.php" class="nav-link"><span class="pcoded-micon"><i class="feather icon-users"></i></span><span class="pcoded-mtext"><?php echo $customer; ?></span></a>
                     </li>
                     <!-- <li class="nav-item pcoded-menu-caption">
 						<label>UI Element</label>
 					</li> -->
                     <li class="nav-item pcoded-hasmenu">
-                        <a class="nav-link"><span class="pcoded-micon"><i
-                                    class="feather icon-file-text"></i></span><span
-                                class="pcoded-mtext"><?php echo $greeting_card; ?></span></a>
+                        <a class="nav-link"><span class="pcoded-micon"><i class="feather icon-file-text"></i></span><span class="pcoded-mtext"><?php echo $greeting_card; ?></span></a>
                         <ul class="pcoded-submenu">
                             <li class=""><a href="./modules/card.php" class="">Card Background</a></li>
                             <li class=""><a href="./modules/card_setting.php" class="">Card Text</a></li>
                         </ul>
                     </li>
                     <li class="nav-item pcoded-hasmenu">
-                        <a class="nav-link"><span class="pcoded-micon"><i class="feather icon-mail"></i></span><span
-                                class="pcoded-mtext"><?php echo $mail; ?></span></a>
+                        <a class="nav-link"><span class="pcoded-micon"><i class="feather icon-mail"></i></span><span class="pcoded-mtext"><?php echo $mail; ?></span></a>
                         <ul class="pcoded-submenu">
                             <li class=""><a href="./modules/mail_log.php" class="">Sending Log</a></li>
                             <li class=""><a href="./modules/mail_handle.php" class="">Send Card Manually</a></li>
@@ -101,8 +94,7 @@ require_once($_SERVER['DOCUMENT_ROOT'] . '/greetingcard/language/language.php');
                         </ul>
                     </li>
                     <li class="nav-item pcoded-hasmenu">
-                        <a class="nav-link"><span class="pcoded-micon"><i class="feather icon-settings"></i></span><span
-                                class="pcoded-mtext"><?php echo $system; ?></span></a>
+                        <a class="nav-link"><span class="pcoded-micon"><i class="feather icon-settings"></i></span><span class="pcoded-mtext"><?php echo $system; ?></span></a>
                         <ul class="pcoded-submenu">
                             <li class=""><a href="./modules/user.php" class="">User Account</a></li>
                             <li class=""><a href="./modules/manager.php" class="">Managers</a></li>
@@ -122,27 +114,15 @@ require_once($_SERVER['DOCUMENT_ROOT'] . '/greetingcard/language/language.php');
             <a href="index.php" class="b-brand">
                 <!-- <img src="./assets/images/logo.svg" alt="" class="logo images"> -->
                 <h5 class="logo images text-white mb-0"><?php echo $greeting_card; ?></h5>
-<<<<<<< HEAD
                 <img src="./assets/images/logo-icon.svg" alt="" class="logo-thumb images">
             </a>
         </div>
         <a class="mobile-menu" id="mobile-header" style="cursor:pointer">
             <i class="feather icon-more-horizontal"></i>
         </a>
-        <div class="collapse navbar-collapse">
+        <div class="collapse navbar-collapse" style="height:60px">
             <a href="#!" class="mob-toggler"></a>
             <!-- <ul class="navbar-nav mr-auto">
-=======
-				<img src="./assets/images/logo-icon.svg" alt="" class="logo-thumb images">
-			</a>
-		</div>
-		<a class="mobile-menu" id="mobile-header" style="cursor:pointer" >
-			<i class="feather icon-more-horizontal"></i>
-		</a>
-		<div class="collapse navbar-collapse" style="height:60px">
-			<a href="#!" class="mob-toggler"></a>
-			<!-- <ul class="navbar-nav mr-auto">
->>>>>>> 772e0113ec0a7d61adbff925d6461728ab2d980c
 				<li class="nav-item">
 					<div class="main-search open">
 						<div class="input-group">
@@ -157,12 +137,10 @@ require_once($_SERVER['DOCUMENT_ROOT'] . '/greetingcard/language/language.php');
 					</div>
 				</li>
 			</ul> -->
-<<<<<<< HEAD
             <ul class="navbar-nav ml-auto">
-                <li>
+                <li style="line-height:0">
                     <div class="dropdown">
-                        <a class="dropdown-toggle" href="#" data-toggle="dropdown"><i
-                                class="icon feather icon-bell"></i></a>
+                        <a class="dropdown-toggle" href="#" data-toggle="dropdown"><i class="icon feather icon-bell"></i></a>
                         <div class="dropdown-menu dropdown-menu-right notification">
                             <div class="noti-head">
                                 <h6 class="d-inline-block m-b-0">Notifications</h6>
@@ -177,11 +155,9 @@ require_once($_SERVER['DOCUMENT_ROOT'] . '/greetingcard/language/language.php');
                                 </li>
                                 <li class="notification">
                                     <div class="media">
-                                        <img class="img-radius" src="./assets/images/user_avatar.jpg"
-                                            alt="Generic placeholder image">
+                                        <img class="img-radius" src="./assets/images/user_avatar.jpg" alt="Generic placeholder image">
                                         <div class="media-body">
-                                            <p><strong>John Doe</strong><span class="n-time text-muted"><i
-                                                        class="icon feather icon-clock m-r-10"></i>5 min</span></p>
+                                            <p><strong>John Doe</strong><span class="n-time text-muted"><i class="icon feather icon-clock m-r-10"></i>5 min</span></p>
                                             <p>New ticket Added</p>
                                         </div>
                                     </div>
@@ -191,55 +167,45 @@ require_once($_SERVER['DOCUMENT_ROOT'] . '/greetingcard/language/language.php');
                                 </li>
                                 <li class="notification">
                                     <div class="media">
-                                        <img class="img-radius" src="./assets/images/user_avatar.jpg"
-                                            alt="Generic placeholder image">
+                                        <img class="img-radius" src="./assets/images/user_avatar.jpg" alt="Generic placeholder image">
                                         <div class="media-body">
-                                            <p><strong>Joseph William</strong><span class="n-time text-muted"><i
-                                                        class="icon feather icon-clock m-r-10"></i>10 min</span></p>
+                                            <p><strong>Joseph William</strong><span class="n-time text-muted"><i class="icon feather icon-clock m-r-10"></i>10 min</span></p>
                                             <p>Prchace New Theme and make payment</p>
                                         </div>
                                     </div>
                                 </li>
                                 <li class="notification">
                                     <div class="media">
-                                        <img class="img-radius" src="./assets/images/user_avatar.jpg"
-                                            alt="Generic placeholder image">
+                                        <img class="img-radius" src="./assets/images/user_avatar.jpg" alt="Generic placeholder image">
                                         <div class="media-body">
-                                            <p><strong>Sara Soudein</strong><span class="n-time text-muted"><i
-                                                        class="icon feather icon-clock m-r-10"></i>12 min</span></p>
+                                            <p><strong>Sara Soudein</strong><span class="n-time text-muted"><i class="icon feather icon-clock m-r-10"></i>12 min</span></p>
                                             <p>currently login</p>
                                         </div>
                                     </div>
                                 </li>
                                 <li class="notification">
                                     <div class="media">
-                                        <img class="img-radius" src="./assets/images/user_avatar.jpg"
-                                            alt="Generic placeholder image">
+                                        <img class="img-radius" src="./assets/images/user_avatar.jpg" alt="Generic placeholder image">
                                         <div class="media-body">
-                                            <p><strong>Joseph William</strong><span class="n-time text-muted"><i
-                                                        class="icon feather icon-clock m-r-10"></i>30 min</span></p>
+                                            <p><strong>Joseph William</strong><span class="n-time text-muted"><i class="icon feather icon-clock m-r-10"></i>30 min</span></p>
                                             <p>Prchace New Theme and make payment</p>
                                         </div>
                                     </div>
                                 </li>
                                 <li class="notification">
                                     <div class="media">
-                                        <img class="img-radius" src="./assets/images/user_avatar.jpg"
-                                            alt="Generic placeholder image">
+                                        <img class="img-radius" src="./assets/images/user_avatar.jpg" alt="Generic placeholder image">
                                         <div class="media-body">
-                                            <p><strong>Sara Soudein</strong><span class="n-time text-muted"><i
-                                                        class="icon feather icon-clock m-r-10"></i>1 hour</span></p>
+                                            <p><strong>Sara Soudein</strong><span class="n-time text-muted"><i class="icon feather icon-clock m-r-10"></i>1 hour</span></p>
                                             <p>currently login</p>
                                         </div>
                                     </div>
                                 </li>
                                 <li class="notification">
                                     <div class="media">
-                                        <img class="img-radius" src="./assets/images/user_avatar.jpg"
-                                            alt="Generic placeholder image">
+                                        <img class="img-radius" src="./assets/images/user_avatar.jpg" alt="Generic placeholder image">
                                         <div class="media-body">
-                                            <p><strong>Joseph William</strong><span class="n-time text-muted"><i
-                                                        class="icon feather icon-clock m-r-10"></i>2 hour</span></p>
+                                            <p><strong>Joseph William</strong><span class="n-time text-muted"><i class="icon feather icon-clock m-r-10"></i>2 hour</span></p>
                                             <p>Prchace New Theme and make payment</p>
                                         </div>
                                     </div>
@@ -266,112 +232,12 @@ require_once($_SERVER['DOCUMENT_ROOT'] . '/greetingcard/language/language.php');
                             </div>
 
                             <ul class="pro-body">
-=======
-			<ul class="navbar-nav ml-auto">
-				<li style="line-height:0">
-					<div class="dropdown">
-						<a class="dropdown-toggle" href="#" data-toggle="dropdown"><i class="icon feather icon-bell"></i></a>
-						<div class="dropdown-menu dropdown-menu-right notification">
-							<div class="noti-head">
-								<h6 class="d-inline-block m-b-0">Notifications</h6>
-								<div class="float-right">
-									<a href="#!" class="m-r-10">mark as read</a>
-									<a href="#!">clear all</a>
-								</div>
-							</div>
-							<ul class="noti-body">
-								<li class="n-title">
-									<p class="m-b-0">NEW</p>
-								</li>
-								<li class="notification">
-									<div class="media">
-										<img class="img-radius" src="./assets/images/user_avatar.jpg" alt="Generic placeholder image">
-										<div class="media-body">
-											<p><strong>John Doe</strong><span class="n-time text-muted"><i class="icon feather icon-clock m-r-10"></i>5 min</span></p>
-											<p>New ticket Added</p>
-										</div>
-									</div>
-								</li>
-								<li class="n-title">
-									<p class="m-b-0">EARLIER</p>
-								</li>
-								<li class="notification">
-									<div class="media">
-										<img class="img-radius" src="./assets/images/user_avatar.jpg" alt="Generic placeholder image">
-										<div class="media-body">
-											<p><strong>Joseph William</strong><span class="n-time text-muted"><i class="icon feather icon-clock m-r-10"></i>10 min</span></p>
-											<p>Prchace New Theme and make payment</p>
-										</div>
-									</div>
-								</li>
-								<li class="notification">
-									<div class="media">
-										<img class="img-radius" src="./assets/images/user_avatar.jpg" alt="Generic placeholder image">
-										<div class="media-body">
-											<p><strong>Sara Soudein</strong><span class="n-time text-muted"><i class="icon feather icon-clock m-r-10"></i>12 min</span></p>
-											<p>currently login</p>
-										</div>
-									</div>
-								</li>
-								<li class="notification">
-									<div class="media">
-										<img class="img-radius" src="./assets/images/user_avatar.jpg" alt="Generic placeholder image">
-										<div class="media-body">
-											<p><strong>Joseph William</strong><span class="n-time text-muted"><i class="icon feather icon-clock m-r-10"></i>30 min</span></p>
-											<p>Prchace New Theme and make payment</p>
-										</div>
-									</div>
-								</li>
-								<li class="notification">
-									<div class="media">
-										<img class="img-radius" src="./assets/images/user_avatar.jpg" alt="Generic placeholder image">
-										<div class="media-body">
-											<p><strong>Sara Soudein</strong><span class="n-time text-muted"><i class="icon feather icon-clock m-r-10"></i>1 hour</span></p>
-											<p>currently login</p>
-										</div>
-									</div>
-								</li>
-								<li class="notification">
-									<div class="media">
-										<img class="img-radius" src="./assets/images/user_avatar.jpg" alt="Generic placeholder image">
-										<div class="media-body">
-											<p><strong>Joseph William</strong><span class="n-time text-muted"><i class="icon feather icon-clock m-r-10"></i>2 hour</span></p>
-											<p>Prchace New Theme and make payment</p>
-										</div>
-									</div>
-								</li>
-							</ul>
-							<div class="noti-footer">
-								<a href="#!">show all</a>
-							</div>
-						</div>
-					</div>
-				</li>
-				<li>
-					<div class="dropdown drp-user">
-						<a href="#" class="dropdown-toggle" data-toggle="dropdown">
-							<i class="icon feather icon-user"></i>
-						</a>
-						<div class="dropdown-menu dropdown-menu-right profile-notification">
-							<div class="pro-head">
-								<img src="./assets/images/user_avatar.jpg" class="img-radius" alt="User-Profile-Image">
-								<span><?php echo $_SESSION['displayName']; ?></span>
-								<a href="logout.php" class="dud-logout" title="Logout">
-									<i class="feather icon-log-out"></i>
-								</a>
-							</div>
-                            
-							<ul class="pro-body">
->>>>>>> 772e0113ec0a7d61adbff925d6461728ab2d980c
                                 <p class="mb-0 ml-3 text-bold">(*) <?php echo $choose; ?></p>
-                                <li><a onclick="chooseLanguage('en')" class="dropdown-item clang"><img
-                                            src="./assets/images/flags/gb.svg" class="icon-flag" alt="">
+                                <li><a onclick="chooseLanguage('en')" class="dropdown-item clang"><img src="./assets/images/flags/gb.svg" class="icon-flag" alt="">
                                         <?php echo $english; ?></a></li>
-                                <li><a onclick="chooseLanguage('tw')" class="dropdown-item clang"><img
-                                            src="./assets/images/flags/tw.svg" class="icon-flag" alt="">
+                                <li><a onclick="chooseLanguage('tw')" class="dropdown-item clang"><img src="./assets/images/flags/tw.svg" class="icon-flag" alt="">
                                         <?php echo $taiwanese; ?></a></li>
-                                <li><a onclick="chooseLanguage('vn')" class="dropdown-item clang"><img
-                                            src="./assets/images/flags/vn.svg" class="feather icon-flag" alt="">
+                                <li><a onclick="chooseLanguage('vn')" class="dropdown-item clang"><img src="./assets/images/flags/vn.svg" class="feather icon-flag" alt="">
                                         <?php echo $vietnamese; ?></a></li>
                             </ul>
                         </div>
